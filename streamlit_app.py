33f9f876bba54ce189f6b710ccbc02aaaadc341e
SNOWFLAKEBB3 <fish6angela@gmail.com>
2024-09-09 18:47:11 +1000

    Update streamlit_app.py
    add  Python package library - requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,7 @@ if my_dataframe:
 
 else:
     st.success('There are no pending orders right now', icon="👍")
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
